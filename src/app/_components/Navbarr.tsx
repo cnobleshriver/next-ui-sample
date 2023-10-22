037990670b03343983bf80a202d975bf3a158779
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-10-22 13:30:58 -0400

    init

---
+++ src/app/_components/Navbarr.tsx
@@ -0,0 +1,89 @@
+'use client'
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@nextui-org/react";
+import jetlogo from "@/assets/logo.svg";
+import Image from "next/image";
+import youtube from "@/assets/youtube.svg";
+
+export default function Navbarr() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "About",
+    "Contact"
+  ];
+  return (
+  <Navbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      className="fixed top-0 z-50 w-full bg-color-primary-100"
+    >
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <Image src={jetlogo} alt="The Jet Business" width={250} height={250} />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <Image src={jetlogo} alt="The Jet Business" width={250} height={250} />
+        </NavbarBrand>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link color="foreground" href="#" aria-current="page">
+            Showroom
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contact
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Button isIconOnly className="bg-color-primary-300">
+            T
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button isIconOnly className="bg-color-primary-300">
+            I
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button isIconOnly className="bg-color-primary-300" >
+            Y
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
